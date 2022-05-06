8286a79dfa01c2bb52656ba4132093c32d429d7b
Joseph Gagliardo <joey@me.com>
2022-05-06 14:52:34 -0400

    0.0.0

---
+++ install-java.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y nano default-jdk
+wget -q -nc -O gradle.zip 
+https://services.gradle.org/distributions/gradle-5.0-bin.zip
+rm -rf /opt/gradle*
+unzip -q -d /opt gradle.zip
+rm gradle.zip
+
