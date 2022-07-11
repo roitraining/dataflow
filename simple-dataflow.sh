77753bb5b8acd2b178c49a153b0ee1646a798382
Joseph Gagliardo <joegagliardo@gmail.com>
2022-07-11 07:33:45 +0000

    1.0.0

--- simple-dataflow.sh
+++ simple-dataflow.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 export BUCKET=gs://$PROJECT
 export REGION=us-central1
-python3 simple.py --input $BUCKET/regions.csv --output $BUCKET/regions_out \
+/root/apache-beam-2.39.0/bin/python simple.py --input $BUCKET/regions.csv --output $BUCKET/regions_out \
 --temp_location gs://$BUCKET/tmp/ --region $REGION --project $PROJECT --runner DataflowRunner
