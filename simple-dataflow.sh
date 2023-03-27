9eefea93ce010363e026ada0447031d98ed61b04
joegagliardo <joey@me.com>
2023-03-27 18:47:38 +0000

    1.3.0

--- simple-dataflow.sh
+++ simple-dataflow.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-# export PROJECT=your project ID
-# gsutil mb gs://$PROJECT
-# gsutil cp regions.csv gs://$PROJECT
-export BUCKET=gs://$PROJECT
+export PROJECT_ID=$(gcloud config get-value project)
+export BUCKET=gs://$PROJECT_ID
+gsutil mb $BUCKET
+gsutil cp regions.csv $BUCKET
 export REGION=us-central1
-/root/apache-beam-2.40.0/bin/python simple.py --input $BUCKET/regions.csv --output $BUCKET/regions_out \
---temp_location gs://$BUCKET/tmp/ --region $REGION --project $PROJECT --runner DataflowRunner
+/jupyter/.kernels/apache-beam-2.46.0/bin/python simple.py --input $BUCKET/regions.csv --output $BUCKET/regions_out \
+--temp_location $BUCKET/tmp/ --region $REGION --project $PROJECT --runner DataflowRunner
--- simple-local.sh
+++ simple-local.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-/root/apache-beam-2.40.0/bin/python simple.py --input regions.csv --output regions_out
+/jupyter/.kernels/apache-beam-2.46.0/bin/python simple.py --input regions.csv --output regions_out
 
